72f83c38884b64c491034b3fdf39ef21d4014591
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-06-11 01:55:37 -0300

    Update restore_backup.sh

--- dinamica/restore_backup.sh
+++ dinamica/restore_backup.sh
@@ -6,15 +6,13 @@ docker exec dinamica_backup_1 restore 20200425
 
 # update wp-config.php
 docker exec dinamica_backup_1 cat /var/www/html/wp-config.php | grep DB_HOST
-#sed -i s/"define('DB_NAME', '.*');"/"define('DB_NAME', '$MYSQL_ENV_MYSQL_DATABASE');"/g /var/www/html/wp-config.php
 docker exec dinamica_backup_1 sed -i s/"define( 'DB_HOST', '.*');"/"define('DB_HOST', '$mysql:3306');"/g /var/www/html/wp-config.php
 docker exec dinamica_backup_1 cat /var/www/html/wp-config.php | grep DB_HOST
 
 # update wp-content/themes/twentyseventeen/functions.php
 
 docker exec dinamica_backup_1 cat /var/www/html/wp-content/themes/twentyseventeen/functions.php | grep update_option
-
-sed -i 's|#update_option( 'siteurl'*|ServerActive='$SERVER_IP'|' ~/environment/zabbix_agent/conf/zabbix_agentd.conf
+docker exec dinamica_backup_1 sed -i 's|//update_option( 'siteurl',.*|update_option( 'siteurl', '$(curl checkip.amazonaws.com)' );|' /var/www/html/wp-content/themes/twentyseventeen/functions.php
 
 
 sed -i 's|ServerActive=.*|ServerActive='$SERVER_IP'|' ~/environment/zabbix_agent/conf/zabbix_agentd.conf
