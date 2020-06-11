7a98b16c25b5d8d9d4c297ee24807757fac5c024
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-06-11 02:17:44 -0300

    Update restore_backup.sh

--- dinamica/restore_backup.sh
+++ dinamica/restore_backup.sh
@@ -4,18 +4,25 @@
 docker exec dinamica_backup_1 tar -xvf backup.tar --directory /backups/
 docker exec dinamica_backup_1 restore 20200425
 
+SERVER_IP=$(curl checkip.amazonaws.com)
+echo $SERVER_IP
+
 # update wp-config.php
 docker exec dinamica_backup_1 cat /var/www/html/wp-config.php | grep DB_HOST
-docker exec dinamica_backup_1 sed -i s/"define( 'DB_HOST', '.*');"/"define('DB_HOST', '$mysql:3306');"/g /var/www/html/wp-config.php
+docker exec dinamica_backup_1 sed -i s/"define( 'DB_HOST', '.*');"/"define( 'DB_HOST', 'mysql:3306');"/g /var/www/html/wp-config.php
 docker exec dinamica_backup_1 cat /var/www/html/wp-config.php | grep DB_HOST
 
 # update wp-content/themes/twentyseventeen/functions.php
 
 docker exec dinamica_backup_1 cat /var/www/html/wp-content/themes/twentyseventeen/functions.php | grep update_option
+# //update_option( 'siteurl', 'http://54.165.165.218' );
+# //update_option( 'home', 'http://54.165.165.218' );
+
+# TO DO
 docker exec dinamica_backup_1 sed -i 's|//update_option( 'siteurl',.*|update_option( 'siteurl', '$(curl checkip.amazonaws.com)' );|' /var/www/html/wp-content/themes/twentyseventeen/functions.php
+docker exec dinamica_backup_1 sed -i s|"//update_option( 'siteurl', '.*' );"|"//update_option( 'siteurl', '$SERVER_IP' );"|g /var/www/html/wp-content/themes/twentyseventeen/functions.php
+docker exec dinamica_backup_1 sed 's_//update_option( 'siteurl', 'http://54.165.165.218' );_//update_option( 'siteurl', '$SERVER_IP' );_' /var/www/html/wp-content/themes/twentyseventeen/functions.php
 
-
-sed -i 's|ServerActive=.*|ServerActive='$SERVER_IP'|' ~/environment/zabbix_agent/conf/zabbix_agentd.conf
-
-update_option( 'siteurl', '$(curl checkip.amazonaws.com)' );
-update_option( 'home', '$(curl checkip.amazonaws.com)' );
+docker exec dinamica_backup_1 cat /var/www/html/wp-content/themes/twentyseventeen/functions.php | grep update_option
+#update_option( 'siteurl', '$(curl checkip.amazonaws.com)' );
+#update_option( 'home', '$(curl checkip.amazonaws.com)' );
