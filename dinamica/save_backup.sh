5243450c6a3d80fee7bfee5c84ddf3f76f0717d1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-10-07 23:39:28 -0300

    Update save_backup.sh

--- dinamica/save_backup.sh
+++ dinamica/save_backup.sh
@@ -4,6 +4,8 @@ docker exec dinamica_backup_1 backup
 # https://docs.docker.com/storage/volumes/#backup-restore-or-migrate-data-volumes
 docker run --rm --volumes-from dinamica_backup_1 -v $(pwd):/backup tonanuvem/wordpress-backup tar cvf /backup/backup.tar /backups
 sudo docker build -t wordpress-backup .
+TAG=$(date '+%Y%m%d_%H%M')
+echo "TAG = $TAG"
 docker login
-docker tag wordpress-backup:latest tonanuvem/wordpress-backup
-docker push tonanuvem/wordpress-backup
+docker tag wordpress-backup:latest tonanuvem/wordpress-backup:$TAG
+docker push tonanuvem/wordpress-backup:$TAG
